b5d51724715b747509a280057e4569054192b03c
CaelanRT <caelanrt15@gmail.com>
2024-12-01 17:51:25 -0500

    Committing this project

---
+++ Book.java
@@ -0,0 +1,64 @@
+public class Book {
+    //instance variables
+    private String title;
+    private String author;
+    private String description;
+    private String publishDate;
+    private boolean inStock;
+
+    //Constructors
+    public Book() {
+
+    };
+
+    public Book(String title, String author, String description, String publishDate, boolean inStock) {
+        this.title = title;
+        this.author = author;
+        this.description = description;
+        this.publishDate = publishDate;
+        this.inStock = inStock;
+    };
+
+    //Methods
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author){
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public String getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(String publishDate){
+        this.publishDate = publishDate;
+    }
+
+    public boolean getInStock() {
+        return inStock;
+    }
+
+    public void setInStock(boolean inStock){
+        this.inStock = inStock;
+    }
+
+
+}
---
+++ Driver.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+
+public class Driver {
+    public static void main(String[] args) {
+        //declare variables and instantiate classes
+        Scanner input = new Scanner(System.in);
+        String userQuit = "N";
+
+        while (!userQuit.equals("Y")) {
+            System.out.println("Welcome to the Library\n");
+            
+        }
+
+        // book.setAuthor("Me");
+
+        // String author = book.getAuthor();
+
+        // System.out.println(author);
+
+
+
+        
+    }
+}
