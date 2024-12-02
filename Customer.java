42f6b0b58cddebd5cae9478c92321db1777af3ff
CaelanRT <caelanrt15@gmail.com>
2024-12-01 20:36:40 -0500

    Commit 3 - adding mainline logic to driver class

--- Customer.java
+++ Customer.java
@@ -1,3 +1,56 @@
+import java.util.ArrayList;
+
 public class Customer {
+    //Instance Variables
+    private String name;
+    private String address;
+    private int numBooksBorrowed;
+    //private ArrayList<Book> listBooksBorrowed = new ArrayList<Book>();
+
+    //constructors
+    public Customer() {
+
+    }
+
+    public Customer(String name, String address, int numBooksBorrowed, ArrayList<Book> listBooksBorrowed) {
+        this.name = name;
+        this.address = address;
+        this.numBooksBorrowed = numBooksBorrowed;
+       // this.listBooksBorrowed = listBooksBorrowed;
+    }
+
+    //Methods
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
     
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getNumBooksBorrowed() {
+        return numBooksBorrowed;
+    }
+
+    public void setNumBooksBorrowed(int numBooksBorrowed) {
+        this.numBooksBorrowed = numBooksBorrowed;
+    }
+
+    // public ArrayList<Book> getListBooksBorrowed() {
+    //     return listBooksBorrowed;
+    // }
+
+    //haven't quite figured out how to add things to an array list that is private in this manner
+    // public void addListBooksBorrowed() {
+
+    // }
+
 }
--- Driver.java
+++ Driver.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 
 public class Driver {
@@ -6,17 +7,53 @@ public class Driver {
         //declare variables and instantiate classes
         Scanner input = new Scanner(System.in);
         String userQuit = "N";
+        int menuChoice = 0;
+
+        System.out.println("Welcome to the Library\n");
+        System.out.println("What is your name?\n");
+        String userName = input.nextLine();
+        Customer customer = new Customer();
+        customer.setName(userName);
+
 
         while (!userQuit.equals("Y")) {
-            System.out.println("Welcome to the Library\n");
+            String gettingName = customer.getName();
+
+            System.out.printf("Hi %s, Please select from the following options:\n 1 - create a book\n 2 - take out a book\n 3 - return a book\n 4 - print a receipt", gettingName);
             
+            if (input.hasNextInt()) {
+                menuChoice = input.nextInt();
+            } else {
+                System.out.println("Invalid input.\n");
+                System.out.println("Please select from the following options:\n 1 - create a book\n 2 - take out a book\n 3 - return a book\n 4 - print a receipt");
+            }
+            input.nextLine();
+
+
+
+            switch (menuChoice) {
+                case 1:
+                    System.out.println("Creating book");
+                    break;
+                
+                case 2:
+                    System.out.println("taking out a book");
+                    break;
+
+                case 3:
+                    System.out.println("returning a book");
+                    break;
+
+                case 4:
+                    System.out.println("printing a receipt");
+
+                default:
+                    System.out.println("Not a valid input, input a number");
+            }
+
+            System.out.println("Do you wish to quit? If so, type Y.");
+            userQuit = input.nextLine().toUpperCase();
         }
-
-        // book.setAuthor("Me");
-
-        // String author = book.getAuthor();
-
-        // System.out.println(author);
 
 
 
